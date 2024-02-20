0bd1ee4105d9f60e540435b2e59f4e349c383c68
Jonny Brownrigg <jonny-uk@hotmail.com>
2024-02-20 12:51:23 +0000

    added logo and sign up page

---
+++ MobileAcebook/SignUpPageView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpPageView.swift
+//  MobileAcebook
+//
+//  Created by Jonny Brownrigg on 20/02/2024.
+//
+
+import SwiftUI
+
+struct SignUpPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpPageView()
+}
